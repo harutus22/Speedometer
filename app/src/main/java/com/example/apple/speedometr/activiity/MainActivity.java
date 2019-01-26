0071f2a8d7bf0cb8772c74925a46c00dda696b04
harutus22 <harutus22@yahoo.com>
2019-01-26 05:23:54 +0400

    speed

--- app/src/main/java/com/example/apple/speedometr/activiity/MainActivity.java
+++ app/src/main/java/com/example/apple/speedometr/activiity/MainActivity.java
@@ -1,16 +1,49 @@
 package com.example.apple.speedometr.activiity;
 
+import android.annotation.SuppressLint;
+import android.app.Notification;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Looper;
+import android.os.Message;
+import android.os.Messenger;
+import android.os.RemoteException;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.apple.speedometr.R;
 import com.example.apple.speedometr.customview.MyCustomView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private MyCustomView myCustomView;
+    private Button accelerate;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        myCustomView = this.findViewById(R.id.speedometer);
+//        accelerate = findViewById(R.id.gasBtn);
+//        myCustomView.setButton(accelerate);
+
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        try {
+            myCustomView.onTouch(accelerate, event);
+        }
+        catch (java.lang.NullPointerException e){
+            Toast.makeText(getApplicationContext(), "Push 'Gas' Button", Toast.LENGTH_LONG).show();
+        }
+        return true;
     }
 }
--- app/src/main/java/com/example/apple/speedometr/customview/MyCustomView.java
+++ app/src/main/java/com/example/apple/speedometr/customview/MyCustomView.java
@@ -4,31 +4,54 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
-import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.RadialGradient;
-import android.graphics.Rect;
 import android.graphics.RectF;
 import android.graphics.Shader;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.apple.speedometr.R;
 
-public class MyCustomView extends View {
+public class MyCustomView extends View implements View.OnTouchListener {
 
-    private Paint OvalPaint,linePaint, backgrounPaint, numberPaint;
+    private Paint OvalPaint,linePaint, backgrounPaint, numberPaint, needleScrewPaint, pedal;
     private RectF oval, background;
-    private int MAX_SPEED = 100;
+    private final int MAX_SPEED = 100;
+    private final int MIN_SPEED = 0;
     private int mMaxSpeed, mCurrentSpeed;
-    private int mainXLineStart = 330, mainXLineEnd = 280, mainYLineStart = 1130, mainYLineEnd = 1200;
     private int mWidth, mHeight;
-    private Paint needleLinePaint;
-    private Paint needleScrewPaint;
-    private Path linePath;
+    private Path linePath = new Path();
+    Canvas canvasRe = new Canvas();
 
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+            switch (event.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    Toast.makeText(getContext(), "isTouched", Toast.LENGTH_LONG).show();
+                    new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            linePath.lineTo(mWidth - 200, mHeight - 50);
+                            canvasRe.drawPath(linePath, linePaint);
+                            canvasRe.rotate(30);
+                            invalidate();
+                        }
+                    }).start();
+
+                    break;
+                case MotionEvent.ACTION_UP:
+                    Toast.makeText(getContext(), "Revealed", Toast.LENGTH_LONG).show();
+                    break;
+            }
+            return true;
+    }
 
     public MyCustomView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
@@ -72,22 +95,26 @@ public class MyCustomView extends View {
         needleScrewPaint.setAntiAlias(true);
         needleScrewPaint.setShader(new RadialGradient(15.0f, 15.0f, 10.0f,
                 Color.DKGRAY, Color.BLACK, Shader.TileMode.CLAMP));
+
+        pedal = new Paint(Paint.ANTI_ALIAS_FLAG);
+        pedal.setColor(Color.GRAY);
     }
+
 
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-
+        canvas.save();
         drawLines(canvas);
         drawNumbers(canvas);
         drawCursor(canvas);
         drawElectricCounter(canvas);
-
+        drawPedal(canvas);
     }
 
     private void drawLines(Canvas canvas) {
-
+        canvasRe = canvas;
         mHeight = canvas.getHeight() / 2;
         mWidth = canvas.getWidth() / 2;
         oval.set(mWidth - 500, mHeight - 500, mWidth + 500, mHeight + 500);
@@ -133,16 +160,25 @@ public class MyCustomView extends View {
     }
 
     private void drawCursor(Canvas canvas) {
-        linePath = new Path();
         linePath.moveTo(mWidth, mHeight);
-        linePath.lineTo(mWidth + 200 , mHeight - 150);
+        linePath.lineTo(mWidth - 250 , mHeight);
         linePath.addCircle(mWidth, mHeight, 10.0f, Path.Direction.CW);
         linePath.close();
+        canvas.restore();
         canvas.drawPath(linePath, linePaint);
 
         canvas.drawCircle(15.0f, 10.0f, 16.0f, needleScrewPaint);
     }
 
     private void drawElectricCounter(Canvas canvas) {
+    }
+
+    private void drawPedal(Canvas canvas) {
+        RectF rectF = new RectF();
+        rectF.set(20, mHeight + mHeight *3/4 , 150, getBottom() + 150);
+        canvas.save();
+        canvas.drawRect(rectF, pedal);
+        canvas.restore();
+        canvas.drawText("Gas", 30, mHeight + mHeight *3/4 + 100, numberPaint);
     }
 }
